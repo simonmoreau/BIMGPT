6ba3cb0861efcbb8f71b0ed6ddf6a1d7d3a93af8
Simon Moreau <simon@bim42.com>
2025-02-15 16:53:43 +0100

    Intercept the response

--- src/BIMGPT.Client/Pages/Home.razor.cs
+++ src/BIMGPT.Client/Pages/Home.razor.cs
@@ -1,13 +1,22 @@
 ﻿using DevExpress.AIIntegration.Blazor.Chat;
+using DevExpress.Utils.Design;
+using Microsoft.AspNetCore.Components;
 using Microsoft.Extensions.AI;
+using Microsoft.Identity.Client;
+using OpenAI.Chat;
 
 namespace BIMGPT.Client.Pages
 {
     public partial class Home
     {
+        [Inject]
+        public IChatClient _chatClient { get; set; }
+
         async Task MessageSent(MessageSentEventArgs args)
         {
-            await args.Chat.SendMessage($"Processed: {args.Content}", ChatRole.Assistant);
+            Microsoft.Extensions.AI.ChatCompletion response = await _chatClient.CompleteAsync(args.Content.ToString());
+            
+            await args.Chat.SendMessage(response.Message.Text, ChatRole.Assistant);
         }
     }
 }
