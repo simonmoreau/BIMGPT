449596c117112d05774d409a3e0cd26905972f28
Simon Moreau <simon@bim42.com>
2025-02-15 15:42:47 +0100

    setup devExpress

--- src/BIMGPT.Client/Program.cs
+++ src/BIMGPT.Client/Program.cs
@@ -1,3 +1,4 @@
+using DevExpress.Blazor;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -13,6 +14,10 @@ namespace BIMGPT.Client
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+            builder.Services.AddDevExpressBlazor(options =>
+            {
+                options.BootstrapVersion = DevExpress.Blazor.BootstrapVersion.v5;
+            });
             await builder.Build().RunAsync();
         }
     }
