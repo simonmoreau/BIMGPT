7c1bf01827c78fcd4c4f23ea2cf6319f7e48d4d4
Simon Moreau <simon@bim42.com>
2025-02-15 16:59:11 +0100

    Setup the conversation

--- src/BIMGPT.Client/Pages/Home.razor.cs
+++ src/BIMGPT.Client/Pages/Home.razor.cs
@@ -3,7 +3,9 @@ using DevExpress.Utils.Design;
 using Microsoft.AspNetCore.Components;
 using Microsoft.Extensions.AI;
 using Microsoft.Identity.Client;
+using Microsoft.VisualBasic;
 using OpenAI.Chat;
+using System.ServiceModel.Channels;
 
 namespace BIMGPT.Client.Pages
 {
@@ -12,10 +14,23 @@ namespace BIMGPT.Client.Pages
         [Inject]
         public IChatClient _chatClient { get; set; }
 
+        private readonly List<Microsoft.Extensions.AI.ChatMessage> _conversation = [];
+
+        protected override async Task OnInitializedAsync()
+        {
+            _conversation.Add(
+                new(ChatRole.System, "You are a product review assistant. Your job is to help people write great product reviews. Keep asking questions on the person's experience with the product until you have enough information to write a review. Then write the review for them and ask if they are happy with it.")
+            );
+
+        }
         async Task MessageSent(MessageSentEventArgs args)
         {
-            Microsoft.Extensions.AI.ChatCompletion response = await _chatClient.CompleteAsync(args.Content.ToString());
-            
+            _conversation.Add(new Microsoft.Extensions.AI.ChatMessage(ChatRole.User, args.Content.ToString()));
+
+            Microsoft.Extensions.AI.ChatCompletion response = await _chatClient.CompleteAsync(_conversation);
+
+            _conversation.Add(response.Message);
+
             await args.Chat.SendMessage(response.Message.Text, ChatRole.Assistant);
         }
     }
